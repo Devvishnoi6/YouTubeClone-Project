ca987218cf7a2381541bf4de5ffff78780d40693
Devvishnoi6 <devvishnoi.0618@gmail.com>
2024-12-23 14:03:26 +0530

    Update axiosConfig.js

--- YouTubeClone-main/frontend/src/utils/axiosConfig.js
+++ YouTubeClone-main/frontend/src/utils/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://youtubeclone-project-backend.onrender.com",
+  baseURL: "http://localhost:5000/api",
 });
