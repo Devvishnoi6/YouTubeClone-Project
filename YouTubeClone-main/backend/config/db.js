6e83815dcb338abfaaa47041e7909f93c12ad196
devvishnoi <devvishnoi.6@gmail.com>
2024-12-23 14:25:08 +0530

    commit

--- YouTubeClone-main/backend/config/db.js
+++ YouTubeClone-main/backend/config/db.js
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000 // Increase the timeout
     });
     console.log('MongoDB Connected');
   } catch (error) {
